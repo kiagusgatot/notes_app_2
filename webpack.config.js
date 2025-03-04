45733866a7bc32a1511342bac70718424f3b62b7
Ki Agus Gatot Mahendra Setiawan <kiagusgatot@gmail.com>
2025-03-04 23:38:33 +0700

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    client: {
+      overlay: {
+        errors: true,
+        warnings: true,
+      },
+    },
+  },
+  mode: 'development',
+};
